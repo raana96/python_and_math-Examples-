ef9d7f0acd2704fe08bb931db2e79237d33ad7e9
raana96 <raanat99@gmail.com>
2024-05-08 09:37:17 +0330

    Update vector.py
    Showing vector

--- Vector/vector.py
+++ Vector/vector.py
@@ -4,10 +4,24 @@ import matplotlib.pyplot as plt
 
 # Vector values
 a = np.array([10,15])
-b = np.array ([15,20])
+b = np.array ([20,20])
+print ( a , b)
 
 # Vector
 plt.quiver (a[0],a[1] , angles='xy' , scale_units='xy' , scale=1)
-plt.xlim ( -1 , 5)
-plt.ylim ( 0 , 5)
-plt.text ( a[0],a[1] , r'$\vec{u}$' , size=25 )
+plt.xlim ( -10 , 20)
+plt.ylim ( -10, 20)
+
+plt.text ( a[0],a[1] , r'$\vec{a}$' , size=25 )
+
+plt.quiver (b[0],b[1] , angles='xy' , scale_units='xy' , scale=1)
+plt.text ( b[0],b[1] , r'$\vec{b}$' , size=25 )
+
+#OP
+c = a - b
+print ( c)
+
+plt.quiver ( c[0],c[1] , angles='xy' , scale_units='xy' , scale=1, color="red" )
+plt.text ( c[0],c[1] , r'$\vec{c}$' , size=25 )
+
+plt.show()
