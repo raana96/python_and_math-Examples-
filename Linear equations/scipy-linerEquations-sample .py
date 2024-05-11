961df25f13e76845f0fd525e7f63d96dc3a01af6
raana96 <raanat99@gmail.com>
2024-05-11 14:50:25 +0330

    liner Equations
    symbols ,   scipy.linalg , numpy

---
+++ Linear equations/scipy-linerEquations-sample .py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy.linalg import solve
+
+# 2x + 3y = 7 
+# 3x + 4y = 9
+
+A = np.array([[ 2 , 3],[ 3 , 4 ]])
+b = np.array([7 , 9])
+
+c = solve(A,b)
+print(c)
+
+import matplotlib.pyplot as plt
+
+x = np.linspace(-5,5,10)
+y1 = ((2*x)-7) / 3
+y2 = ((3*x)-9) / 4
+plt.plot(x , y1)
+plt.plot(x,y2)
+plt.show()
